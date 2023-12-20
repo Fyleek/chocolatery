2de5a7479c81889165f78696da09139b770e7cac
Decloquement Oscar <odecloquement@gmail.com>
2023-12-20 02:01:31 +0100

    Update .NET Install & Use chocolatey

--- chocolatey.bat
+++ chocolatey.bat
@@ -1,36 +1,65 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-:: Check if .NET Framework 4.8 is installed
-echo Checking for .NET Framework 4.8...
-powershell -Command "& { if (-not (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' -Name Release | Where-Object { $_.Release -ge 528040 })) { Write-Host '.NET Framework 4.8 not found, installation required.'; Exit 1 } else { Write-Host '.NET Framework 4.8 is already installed.' } }"
+:: Define the minimum release number for .NET Framework 4.8
+SET "minReleaseNumber=528040"
+:: Define the chocolatey executable
+SET choco="C:\ProgramData\chocolatey\bin\choco.exe"
 
-IF %ERRORLEVEL% NEQ 0 (
-    REM Specify the version of .NET you want to install
-    set DOTNET_VERSION=6.0
-    REM Display message to let user know that installation is in progress
-    echo Installing .NET Framework %DOTNET_VERSION%...
-    REM Download dotnet-install.ps1
-    powershell -command "& { iwr -useb https://dot.net/v1/dotnet-install.ps1 -outf dotnet-install.ps1 }"
-    REM Run dotnet-install.ps1 to install .NET
-    powershell -command "& { .\dotnet-install.ps1 -Channel LTS -Version %DOTNET_VERSION% -InstallDir %UserProfile%\dotnet }"
-    REM Delete dotnet-install.ps1
-    del dotnet-install.ps1
-    REM Display the installed .NET version (optional)
-    echo .NET Framework %DOTNET_VERSION% is installed
-dotnet --version
+:: Check if .NET Framework 4.8 or higher is installed
+echo Checking for .NET Framework 4.8 or higher...
+REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v Release > nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    FOR /F "tokens=3" %%G IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v Release') DO SET "dotNetVersion=%%G"
+    IF !dotNetVersion! GEQ %minReleaseNumber% (
+        echo .NET Framework 4.8 or later is installed.
+    ) ELSE (
+        echo .NET Framework 4.8 or later is not installed.
+        goto InstallError
+    )
 ) ELSE (
-    echo .NET Framework version is compliant
+    echo Unable to determine the .NET Framework version installed.
+    goto InstallError
 )
 
 :: Install Chocolatey
 echo Installing Chocolatey...
-powershell -Command "& { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) }"
+powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+if errorlevel 1 goto InstallError
 
-:: Install software using Chocolatey
-echo Installing software...
-@REM powershell -Command "& { choco install software1; choco install software2; choco install software3; }"
-REM Replace software1, software2, software3 with the Chocolatey package names of your software.
+:: Check if Chocolatey is installed
+echo Checking for Chocolatey installation...
+%choco% --version > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Chocolatey is not installed. Please install it first.
+    goto InstallError
+)
 
-echo Installation completed.
+:: Chocolatey is installed, execute your commands here
+echo Chocolatey is installed. Executing commands...
+
+:: Example Chocolatey command
+@REM choco install somepackage -y
+@REM IF %ERRORLEVEL% NEQ 0 (
+@REM     echo Error occurred while installing somepackage.
+@REM     goto InstallError
+)
+
+:: Add more Chocolatey commands here
+:: ...
+
+:: Checking if errors have occurred in PowerShell
+if errorlevel 1 (
+    echo An error occurred while running commands in PowerShell.
+    goto InstallError
+)
+
+goto EndScript
+
+:InstallError
+echo An error occurred during the installation. Please check the error message above.
+
+:EndScript
+echo Script execution finished. Press any key to exit.
+pause >nul
 ENDLOCAL
