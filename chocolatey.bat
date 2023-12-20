08bc7a371b0e990f1b031d0623758ea4d9886b43
Decloquement Oscar <odecloquement@gmail.com>
2023-12-20 14:25:21 +0100

    Update script

--- chocolatey.bat
+++ chocolatey.bat
@@ -3,8 +3,11 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 :: Define the minimum release number for .NET Framework 4.8
 SET "minReleaseNumber=528040"
-:: Define the chocolatey executable
-SET choco="C:\ProgramData\chocolatey\bin\choco.exe"
+
+:: List of software to install with their version
+SET "packages=googlechrome:119.0.6045.160 figma:116.12.2 python3:3.12.1 notepadplusplus.install:8.6.0 git.install:2.43.0 visualstudio2022professional:117.8.3.0 notion:2.2.0 vscode:1.85.1 mysql.workbench:8.0.34 pycharm:2023.3.1 postman:10.19.7 nodejs:21.4.0 nvm:1.1.12 powertoys:0.76.2"
+:: Variable to store installation failures
+SET "failedInstalls="
 
 :: Check if .NET Framework 4.8 or higher is installed
 echo Checking for .NET Framework 4.8 or higher...
@@ -27,6 +30,9 @@ echo Installing Chocolatey...
 powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 if errorlevel 1 goto InstallError
 
+:: Define the chocolatey executable
+SET "choco=C:\ProgramData\chocolatey\bin\choco.exe"
+
 :: Check if Chocolatey is installed
 echo Checking for Chocolatey installation...
 %choco% --version > nul 2>&1
@@ -38,15 +44,32 @@ IF %ERRORLEVEL% NEQ 0 (
 :: Chocolatey is installed, execute your commands here
 echo Chocolatey is installed. Executing commands...
 
-:: Example Chocolatey command
-@REM choco install somepackage -y
-@REM IF %ERRORLEVEL% NEQ 0 (
-@REM     echo Error occurred while installing somepackage.
-@REM     goto InstallError
+:: Loop to install each software
+FOR %%i IN (%packages%) DO (
+    FOR /F "tokens=1,2 delims=:" %%a IN ("%%i") DO (
+        SET "pkgName=%%a"
+        SET "pkgVersion=%%b"
+        ECHO Installing !pkgName! version !pkgVersion!...
+        IF "!pkgName!"=="googlechrome" (
+            !choco! install !pkgName! --version !pkgVersion! --ignore-checksums -y
+        ) ELSE IF "!pkgName!"=="figma" (
+            !choco! install !pkgName! --version !pkgVersion! --ignore-checksums -y
+        ) ELSE (
+            !choco! install !pkgName! --version !pkgVersion! -y
+        )
+        IF !ERRORLEVEL! NEQ 0 (
+            ECHO Error occurred while installing !pkgName!.
+            SET "failedInstalls=!failedInstalls! !pkgName!"
+        )
+    )
 )
 
-:: Add more Chocolatey commands here
-:: ...
+:: Display results
+IF NOT "!failedInstalls!"=="" (
+    ECHO The following programs could not be installed: !failedInstalls!
+) ELSE (
+    ECHO All programs were installed successfully.
+)
 
 :: Checking if errors have occurred in PowerShell
 if errorlevel 1 (
